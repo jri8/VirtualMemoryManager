f5786590086be61d48b1bb6f6ef582acc9139766
osboxes.org <Jackson>
2018-02-26 06:47:51 -0500

    reading in from file

--- memoryManager.c
+++ memoryManager.c
@@ -1,10 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+const char* FILE_NAME = "addresses.txt";
+const int BUFFER_SIZE = 256;
 
-int main (int argc, char*[] argv){
+int main (int argc, char* argv[]){
 	
-	if (argc < 2)
+	if (argc < 2){
 		printf("Not enough arguments\nProgram Exiting\n");
+		exit(0);
+	}
 
+	FILE *fd;
+	fd = fopen(argv[1], "r");
+	
+	if (fd == NULL){
+		printf("File failed to open\n");
+		exit(0);
+	}
 
+	char readBuf[BUFFER_SIZE];
+	memset(readBuf, 0, sizeof(readBuf));
+
+	while (fgets(readBuf, BUFFER_SIZE, fd) != NULL){
+		printf("%s\n", readBuf);
+	}
+
+	return 0;
 
 }
