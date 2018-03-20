53abdf59c3adba84fb6cebfc41ba6915d8437ade
osboxes.org <Jackson>
2018-03-19 23:55:55 -0400

    working PageTable

--- makefile
+++ makefile
@@ -2,3 +2,5 @@ MM : m
 	./a.out atest.txt
 m :
 	cc memoryManager.c
+big : m
+	./a.out addresses.txt
--- memoryManager.c
+++ memoryManager.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /*------TODO---------
 Structure:
@@ -38,11 +38,45 @@ const int BUFFER_SIZE = 256;
 const int PHYS_MEM_SIZE = 256;
 const int TLB_SIZE = 16;
 
+int readFromDisk (int pageNum, char *PM, int* OF){
 
-	return 1;
+	char buffer[BUFFER_SIZE];
+	memset(buffer, 0, sizeof(buffer));
+
+	FILE *BS;
+	BS = fopen("BACKING_STORE.bin", "rb");
+	if (BS == NULL){
+		printf("File failed to open\n");
+		exit(0);
+	}
+	
+//	printf("offset in fseek: %d\n", pageNum*PHYS_MEM_SIZE);
+
+	if (fseek(BS, pageNum * PHYS_MEM_SIZE, SEEK_SET)!=0)
+		printf("error in fseek\n");
+
+	if (fread(buffer, sizeof(char), PHYS_MEM_SIZE, BS)==0)
+		printf("error in fread\n");
+	
+
+	int i = 0;
+	for(i; i < PHYS_MEM_SIZE; i++){
+		*((PM+(*OF)*PHYS_MEM_SIZE)+i) = buffer[i];
+	/*	printf("buffer[%d]=%d\n",i+pageNum*256, buffer[i]);*/
+	}
+	
+	(*OF)++;
+
+	return (*OF)-1;
+//	printf("&d\n ", *((PM+i*n)+j));
+	// i = current row row number, n = elements per row, j = colum num
 }
 
-int findPage(int logicalAddr, unsigned char* PT){
+
+
+
+
+int findPage(int logicalAddr, char* PT, char* PM, int* OF){
 
 	unsigned char mask = 0xFF;
 	unsigned char offset;
@@ -52,10 +86,30 @@ int findPage(int logicalAddr, unsigned char* PT){
 	printf("%X\t", pageNum);	
 
 	offset = logicalAddr & mask;
-	printf("%X\n", offset);
+	printf("%X\t", offset);
+	
+	//Check if in TLB
 
+	//Check if in PageTable
 
+	if (PT[pageNum] != NULL)
+		printf("Pagehit\t\t");
+//	PT[pageNum] = offset;
+	
+	
+	//if not in either read from disk
+	else{
+		printf("pageFault\t");
+		PT[pageNum] = readFromDisk(pageNum, PM, OF);
+	}
+	int value = PT[pageNum];
+	int index = (value*PHYS_MEM_SIZE)+offset;
+	value = *(PM+index);
+	printf("value = %d at PM[%d]\n",value,index);	
+	
 	return 0;
+
+
 }
 
 
@@ -65,10 +119,18 @@ int main (int argc, char* argv[]){
 	
 	int val;
 	FILE *fd;
+	int openFrame = 0;
 	
 	unsigned char PageTable[PAGE_TABLE_SIZE];
+//	memset(PageTable, NULL, sizeof(PageTable));	
+	int i =0;
+	for (i;i < PAGE_TABLE_SIZE;i++)
+		PageTable[i] = NULL;
+
 	unsigned char TLB[TLB_SIZE];	
-	unsigned char PhyMem [PHYS_MEM_SIZE][PHYS_MEM_SIZE]; 
+//	memset(TLB, NULL, sizeof(TLB));
+
+	char PhyMem[PHYS_MEM_SIZE][PHYS_MEM_SIZE]; 
 
 	if (argc < 2){
 		printf("Not enough arguments\nProgram Exiting\n");
@@ -84,13 +146,17 @@ int main (int argc, char* argv[]){
 
 	printf("Value\tPageNum\tOffset\n ");	
 	while (fscanf(fd, "%d", &val)==1){
-		printf("%X\t", val);
-		findPage(val, PageTable);	
-		
-	
+		printf("%d\t", val);
+		findPage(val, PageTable, (char*)PhyMem, &openFrame);	
 	}
-//	readFromDisk();
 
+//	readFromDisk(0, (char*)PhyMem, &openFrame);
+	
+	printf("openFrame = %d\n", openFrame);
+/*	int i = 0;
+	for (i; i < PHYS_MEM_SIZE; i++)
+		printf("PhyMem[%d]=%d\n",i, PhyMem[0][i]);
+*/
 	close(fd);
 	return 0;
 
